57d478be07e2653a813b676723e16934e4d5fea3
DJEzio <mariusadrian.popa@ulbsibiu.ro>
2024-07-11 12:36:58 +0300

    Works showing the db contents on page

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,5 +1,6 @@
 using BlazorApp1.Components;
 using BlazorApp1.Data;
+using BlazorApp1.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,8 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddDbContext<ExpenseTrackerContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("ExpenseTrackerDatabase")));
+
+builder.Services.AddScoped<CategoryService>();
 
 var app = builder.Build();
 
---
+++ BlazorApp1/Services/CategoryService.cs
@@ -0,0 +1,50 @@
+﻿using BlazorApp1.Data;
+using BlazorApp1.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Services
+{
+    public class CategoryService
+    {
+        private readonly ExpenseTrackerContext _context;
+
+        public CategoryService(ExpenseTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task AddCategoryAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _context.Entry(category).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
